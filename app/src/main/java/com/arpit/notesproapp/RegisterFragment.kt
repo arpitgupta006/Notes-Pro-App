6bfd3f2913ddb61eeab939244d60325daa457e55
arpitgupta006 <arpitdude006@gmail.com>
2022-12-17 02:05:35 +0530

    Commit4

--- app/src/main/java/com/arpit/notesproapp/RegisterFragment.kt
+++ app/src/main/java/com/arpit/notesproapp/RegisterFragment.kt
@@ -5,17 +5,32 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.arpit.notesproapp.databinding.FragmentRegisterBinding
 
 
 class RegisterFragment : Fragment() {
+
+    private var _binding : FragmentRegisterBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        _binding = FragmentRegisterBinding.inflate(inflater , container , false)
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false)
+        binding.btnSignUp.setOnClickListener {
+            findNavController().navigate(R.id.action_registerFragment_to_mainFragment)
+        }
+        return binding.root
     }
 
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
